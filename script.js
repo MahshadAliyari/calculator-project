7ee792f54444a6260789ce3f3f2879ed6854a835
mahshad <mahshadayari@gmail.com>
2026-08-20 09:41:58 +0300

    add js structure

---
+++ script.js
@@ -0,0 +1,44 @@
+const number1Input=
+document.querySelector("#number1");
+const number2Input=
+document.querySelector("#number2");
+const operatorInput=
+document.querySelector("#operator");
+const calculateButton=
+document.querySelector("#calculate");
+const result=
+document.querySelector("#result");
+
+
+function calculate(a, b, operator) {
+    if (operator === "+") {
+        return a + b;
+    }
+
+    if (operator === "-") {
+        return a - b;
+    }
+
+    if (operator === "*") {
+        return a * b;
+    }
+
+    if (operator === "/") {
+        if (b === 0) {
+            return "Cannot divide by zero";
+        }
+
+        return a / b;
+    }
+}
+
+calculateButton.addEventListener("click", function () {
+
+    const number1 = Number(number1Input.value);
+    const number2 = Number(number2Input.value);
+    const operator = operatorInput.value;
+
+    const answer = calculate(number1, number2, operator);
+
+    result.textContent = answer;
+});
